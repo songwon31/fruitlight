7a1162c03ec51f8897af68379180d72c1c514b99
noismik <www_lol@naver.com>
2023-09-12 17:30:40 +0900

    bug: 오류 수정

--- src/main/java/poris/fruitlight/controller/mobile/AddressController.java
+++ src/main/java/poris/fruitlight/controller/mobile/AddressController.java
@@ -5,33 +5,32 @@ import java.util.List;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import lombok.extern.slf4j.Slf4j;
 import poris.fruitlight.dto.AddressBook;
 import poris.fruitlight.dto.Shopper;
 import poris.fruitlight.service.AddressBookService;
-import poris.fruitlight.util.AlertScript;
 
 
 @Slf4j
-@Controller
+@RestController
 public class AddressController {
 	
 	@Autowired
 	private AddressBookService addrBookService;
-	
-	
+		
 	/**
-	 * @author 고재승
+	 * @author 김시온
 	 * @param model - (받는 사람, 받을 주소, 연락처, 배송 요청사항) DTO
 	 * @return 배송지 목록 페이지 이동
 	 */
 	@RequestMapping("/addressBook")
-	public String AddressBook(HttpSession session, Model model) {
+	public String AddressList(HttpSession session, Model model) {
 		 
 		// Step1. 회원 정보 조회 - 회원 고유번호 획득
 		Shopper shopper = (Shopper) session.getAttribute("ShopperInfo");
@@ -48,12 +47,12 @@ public class AddressController {
 		
 		return "addressBook";
 	}
-	
-	/**
+/*	
+	*//**
 	 * @author 고재승
 	 * @param addressNo - 삭제할 배송지 고유번호
 	 * @return - 배송지 목록 페이지으로 리다이렉트 
-	 */
+	 *//*
 	@GetMapping("/addressBook/deleteAddressBook")
 	public String deleteAddressBook(int addressNo) {
 		addrBookService.deleteAddressBook(addressNo);
@@ -61,10 +60,10 @@ public class AddressController {
 		return "redirect:/addressBook";
 	}
 	
-	/**
+	*//**
 	 * @author 고재승
 	 * @return 배송지 추가 페이지 이동
-	 */
+	 *//*
 	@GetMapping("/addressBook/newAddressBook")
 	public String newAddressBook() {
 		
@@ -72,15 +71,15 @@ public class AddressController {
 	}
 	
 	
-	/**
+	*//**
 	 * @author 고재승
 	 * @return 배송지 추가 페이지 이동
-	 */
+	 *//*
 	@PostMapping("/addressBook/addAddressBook")
 	public String addAddressBook(AddressBook addressBook) {
 		log.info("addressBook :" + addressBook.toString());
 		addrBookService.createAddressBook(addressBook);
 		
 		return "redirect:/addressBook";
-	}
+	}*/
 }
