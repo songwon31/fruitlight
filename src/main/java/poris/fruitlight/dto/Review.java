4e1682599fae9983fa841d82368bbd823772bcad
songwon31 <sws1719@naver.com>
2023-09-13 18:08:48 +0900

    Review DTO 수정

--- src/main/java/poris/fruitlight/dto/Review.java
+++ src/main/java/poris/fruitlight/dto/Review.java
@@ -12,15 +12,15 @@ import lombok.Data;
  */
 @Data
 public class Review {
-	private int reviewNo;				// 리뷰 고유번호
-	private int orderNo;				// 결제 고유번호
-	private int productNo;				// 상품 고유 번호
-	private int boardNo;				// 게시글 고유번호
-	private int starRate;				// 평점 ( 0 ~ 100 )
-	private int helpPoint;				// 평점 ( 0 ~ 100 )
+	private int review_no;				// 리뷰 고유번호
+	private int order_no;				// 결제 고유번호
+	private int product_no;				// 상품 고유 번호
+	private int board_no;				// 게시글 고유번호
+	private int star_rate;				// 평점 ( 0 ~ 100 )
+	private int help_point;				// 평점 ( 0 ~ 100 )
 	private String content;				// 리뷰 내용
-	private String shopperName;			// 리뷰 작성자
-	private String productName;			// 리뷰 작성자
+	private String shopper_name;			// 리뷰 작성자
+	private String product_name;			// 리뷰 작성자
 	@DateTimeFormat(pattern="yyyy-MM-dd")
-	private Date writeDate;				// 리뷰 작성일
+	private Date write_date;				// 리뷰 작성일
 }
