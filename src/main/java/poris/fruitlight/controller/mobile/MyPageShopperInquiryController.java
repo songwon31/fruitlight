5d8df622f0474beacdfea37272aa1ee131a12950
dwdwb <pipal1221@gmail.com>
2023-09-20 18:24:00 +0900

    MyPageShopperInquiryController 상품문의 삭제 기능 구현

--- src/main/java/poris/fruitlight/controller/mobile/MyPageShopperInquiryController.java
+++ src/main/java/poris/fruitlight/controller/mobile/MyPageShopperInquiryController.java
@@ -7,6 +7,8 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -42,6 +44,11 @@ public class MyPageShopperInquiryController {
 	   productInquiry.setPRODUCT_NAME(productName);
 	   List<ProductInquiry> productInquiryList = pageShopperInquiryService.getMShopperInquiryList(productInquiry);
 	   return productInquiryList;
+   }
+   
+   @PostMapping(value="myPageShopperInquiry/deleteInquiry", produces="application/json; charset=UTF-8")
+   public void deleteInquiry(@RequestBody ProductInquiry productInquiry) {
+	   pageShopperInquiryService.deleteShopperInquiry(productInquiry.getINQUIRY_NO());
    }
    
    /**
